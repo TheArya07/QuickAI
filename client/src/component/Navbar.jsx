ef3a230f6e0143f3d09484f5c0a5e6060e4a4f59
Aryan Kumar <iamaryan.hzb@gmail.com>
2025-12-15 17:12:34 +0530

    feat: integrate Clerk for authentication and user management
    - Added @clerk/clerk-react and @clerk/nextjs dependencies to package.json.
    - Updated Navbar component to utilize Clerk's UserButton and user context.
    - Wrapped the application in ClerkProvider in main.jsx, ensuring the use of a publishable key for authentication.

--- client/src/component/Navbar.jsx
+++ client/src/component/Navbar.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import { assets } from '../assets/assets'
 import { useNavigate } from 'react-router-dom'
 import { ArrowRight } from 'lucide-react'
+import {useClerk,UserButton,useUser} from '@clerk/clerk-react'
 
 const Navbar = () => {
   const navigate = useNavigate()
-
+const {user} = useUser()
+const {openSignIn} = useClerk()
   return (
     <div className="fixed z-50 w-full backdrop-blur-2xl flex
       justify-between items-center py-3 px-4 sm:px-20 xl:px-32">
