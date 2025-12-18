5ebbf8e8d2b205a0185c2616928045861d368bd0
Aryan Kumar <iamaryan.hzb@gmail.com>
2025-12-18 18:57:35 +0530

    feat: implement Home page layout with Hero, AiTools, Testimonial, Plan, and Footer components

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
 import Navbar from '../component/Navbar'
-
+import Hero from '../component/Hero'
+import AiTools from '../component/AiTools'
+import Testimonial from '../component/Testimonial'
+import Plan from '../component/Plan'
+import Footer from '../component/Footer'
 const Home = () => {
   return (
     <>
       <Navbar />
       <Hero />
+      <AiTools />
+      <Testimonial />
+      <Plan />
+      <Footer />
     </>
   )
 }
