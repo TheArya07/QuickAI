a24405172025e19914c6f49faf33c68c8cecd20e
Aryan Kumar <iamaryan.hzb@gmail.com>
2025-12-18 11:56:55 +0530

    feat: add Hero component and integrate it into Home page; update Navbar for user authentication

---
+++ client/src/component/Hero.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div classname='px-4 sm:px-20 x1:px32 relative inline-flex flex-col w-full 
+    justify-center bg-[yrl(/gardientBackground.png)] bg-cover bg-n0-repeat
+    min-hscreen'>
+      
+    </div>
+  )
+}
+
+export default Hero
--- client/src/component/Navbar.jsx
+++ client/src/component/Navbar.jsx
@@ -11,19 +11,24 @@ const {openSignIn} = useClerk()
   return (
     <div className="fixed z-50 w-full backdrop-blur-2xl flex
       justify-between items-center py-3 px-4 sm:px-20 xl:px-32">
-
-      <img
+         <img
         src={assets.logo}
         alt="Logo"
         className="w-32 sm:w-44 cursor-pointer"
         onClick={() => navigate('/')}
       />
-
-      <button className="flex items-center gap-2 rounded-full text-sm
+      {
+        user ? <UserButton /> 
+        : 
+        (
+            <button 
+            onClick={openSignIn}className="flex items-center gap-2 rounded-full text-sm
         cursor-pointer bg-primary text-white px-10 py-2.5">
-        Get started
-        <ArrowRight className="w-4 h-4" />
+        Get started<ArrowRight className="w-4 h-4" />
       </button>
+
+        )
+      }
 
     </div>
   )
