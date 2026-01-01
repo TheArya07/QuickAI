b5ab702f7df4a4b697426a6887c1feabc4368269
Aryan Kumar <iamaryan.hzb@gmail.com>
2026-01-01 23:12:17 +0530

    fix: update color codes for Sparkles and button gradient in RemoveBackground component

--- client/src/pages/RemoveBackground.jsx
+++ client/src/pages/RemoveBackground.jsx
@@ -5,9 +5,9 @@ const RemoveBackground = () => {
 
   const [input, setInput] = useState("");
   
-    const onSubmitHandler = async (e) => {
-      e.preventDefault();
-    };
+  const onSubmitHandler = async (e) => {
+    e.preventDefault();
+  };
   
   return (
     <div className="h-full overflow-y-scroll p-6 flex items-start flex-wrap gap-4 text-slate-700">
@@ -17,7 +17,7 @@ const RemoveBackground = () => {
         className="w-full max-w-lg p-4 bg-white rounded-lg border border-gray-200"
       >
         <div className="flex items-center gap-3">
-          <Sparkles className="w-6 text-[#FF4938]" />
+          <Sparkles className="w-6 text-[#2563EB]" />
           <h1 className="text-xl font-semibold">Background Removal</h1>
         </div>
 
@@ -35,7 +35,7 @@ const RemoveBackground = () => {
 
         <button
           className="w-full flex justify-center items-center gap-2
-          bg-gradient-to-r from-[#F6AB41] to-[#FF4938] text-white px-4 py-2 mt-6
+          bg-gradient-to-r from-[#3B82F6] to-[#2563EB] text-white px-4 py-2 mt-6
           text-sm rounded-lg cursor-pointer"
         >
           <Eraser className="w-5" />
@@ -46,10 +46,10 @@ const RemoveBackground = () => {
       {/* RIGHT COLUMN */}
       <div
         className="w-full max-w-lg p-4 bg-white rounded-lg flex flex-col border
-      border-gray-200 min-h-96"
+        border-gray-200 min-h-96"
       >
         <div className="flex items-center gap-3">
-          <Eraser className="w-5 h-5 text-[#FF4938]" />
+          <Eraser className="w-5 h-5 text-[#2563EB]" />
           <h1 className="text-xl font-semibold">Processed Image</h1>
         </div>
 
